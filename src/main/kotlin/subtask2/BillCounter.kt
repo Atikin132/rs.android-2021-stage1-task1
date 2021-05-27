0e0f8de226e7397f584cd346cc74a98c76978a1d
Mikita Kupryianau <nikita.kupriyan0v@yahoo.com>
2021-05-27 16:31:06 +0300

    feat: add BillCounter task

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,22 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+//        throw NotImplementedError("Not implemented")
+        val billArrayList: MutableList<Int> = java.util.ArrayList()
+        for (i in bill) {
+            billArrayList.add(i)
+        }
+        billArrayList.removeAt(k)
+        var sum = 0
+        val difference: Int
+        for (i in billArrayList.indices) {
+            sum += billArrayList.get(i)
+        }
+        difference = b - sum / 2
+        return if (difference == 0) {
+            "bon appetit"
+        } else {
+            difference.toString()
+        }
     }
 }
