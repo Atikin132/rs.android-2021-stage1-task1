abf33edb38554211820803b2dc752e2ba2d64821
Mikita Kupryianau <nikita.kupriyan0v@yahoo.com>
2021-05-28 15:38:14 +0300

    feat: add StringParser task

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,48 @@
 package subtask3
 
+import java.util.*
+
 class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+//        throw NotImplementedError("Not implemented")
+        val inputStringChar = inputString.toCharArray()
+        val strings = ArrayList<String>()
+
+        for (i in inputStringChar.indices) {
+            check(inputStringChar, i, i, '(', ')', strings, inputString)
+            check(inputStringChar, i, i, '[', ']', strings, inputString)
+            check(inputStringChar, i, i, '<', '>', strings, inputString)
+        }
+        return strings.toArray(arrayOfNulls<String>(strings.size))
+    }
+
+    private fun check(
+        inputStringChar: CharArray,
+        i: Int,
+        k: Int,
+        ch: Char,
+        ch1: Char,
+        strings: ArrayList<String>,
+        inputString: String
+    ) {
+        var k = k
+        if (inputStringChar[i] == ch) {
+            while (inputStringChar[k] != ch1) {
+                if (inputStringChar[k + 1] == ch) {
+                    var a = k
+                    while (inputStringChar[a] != ch1) {
+                        a++
+                    }
+                    k = a
+                }
+                k++
+                if (k == inputStringChar.size) {
+                    break
+                }
+            }
+            strings.add(inputString.substring(i + 1, k))
+        }
     }
 }
